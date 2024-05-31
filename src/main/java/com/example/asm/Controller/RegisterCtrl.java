1b206246951c04c0d5cea2fd0eec954603d90433
Nguyen Duc Hao <haoxaomitom@gmail.com>
2024-05-31 09:40:56 +0700

    sixth commit first time, change at register and login

--- src/main/java/com/example/asm/Controller/RegisterCtrl.java
+++ src/main/java/com/example/asm/Controller/RegisterCtrl.java
@@ -1,6 +1,8 @@
 package com.example.asm.Controller;
 
 import com.example.asm.Entity.Account;
+import com.example.asm.Entity.User;
+import com.example.asm.Repository.AccountRepo;
 import com.example.asm.Service.AccountSer;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,24 +12,36 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class RegisterCtrl {
 
     @Autowired
     private AccountSer accountService;
-
-    @GetMapping("/register/form")
+    @Autowired
+    private AccountRepo repo;
+    @GetMapping("/register")
     public String register(Model model) {
         model.addAttribute("register", new Account());
         return "/views/register";
     }
 
     @PostMapping("/register/submit")
-    public String save(@Valid @ModelAttribute("register") Account acc, BindingResult bindingResult) {
+    public String save(@Valid @ModelAttribute("register") Account acc, BindingResult bindingResult, Model model) {
+        Account existingAccount = repo.findByMakh(acc.getMakh());
+        Account exitstingEmail = repo.findByEmail(acc.getEmail());
         if (bindingResult.hasErrors()) {
-            return "/views/register";
+            return "register";
+        }
+        if (existingAccount != null) {
+            model.addAttribute("makh", "Username này đã tồn tại !");
+            return "register";
+        }
+        if (exitstingEmail != null) {
+            model.addAttribute("email", "Email này đã tồn tại !");
+            return "register";
         }
         accountService.register(acc);
         return "redirect:/index";
